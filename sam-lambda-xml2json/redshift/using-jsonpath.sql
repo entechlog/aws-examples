fedb25e0c23fbe819d90fa2bacffa681bffec70f
Siva Nadesan <entechlog@gmail.com>
2021-09-18 13:05:41 -0700

    updated the demo sql for redshift

--- sam-lambda-xml2json/redshift/using-jsonpath.sql
+++ sam-lambda-xml2json/redshift/using-jsonpath.sql
@@ -1,15 +1,19 @@
-DROP TABLE run_results;
-
-CREATE TABLE run_results (
-	dbt_schema_version VARCHAR,
-	dbt_version VARCHAR
+-- Create table
+CREATE TABLE breakfast_menu (
+	food_name VARCHAR,
+	food_price VARCHAR,
+	food_description VARCHAR
 	);
 
-COPY run_results
-FROM 's3://entechlog-test/run_results.json' CREDENTIALS 'aws_iam_role=arn:aws:iam::582805303120:role/dev-entechlog-redshift' json 's3://entechlog-test/jsonpaths.json';
+-- Copy JSON from S3 to Redshift table using json path
+COPY breakfast_menu
+FROM 's3://sam-lambda-xml2json/json/' CREDENTIALS 'aws_iam_role=<iam-role-for-redshift>' json 's3://sam-lambda-xml2json/jsonpath/jsonpath_breakfast_menu.json';
 
+-- Validate record count
 SELECT COUNT(*)
-FROM run_results
+FROM breakfast_menu
 
+-- Validate parsed JSON
 SELECT *
-FROM run_results
+FROM breakfast_menu
+WHERE food_name IS NOT NULL;
