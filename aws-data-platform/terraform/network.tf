d306364938976de89335c8787bce093953d13caa
entechlog <38563714+entechlog@users.noreply.github.com>
2022-10-02 18:26:21 -0700

    changes to create data lookup for aws_availability_zones (#16)

--- aws-data-platform/terraform/network.tf
+++ aws-data-platform/terraform/network.tf
@@ -4,7 +4,7 @@ module "vpc" {
   env_code                  = var.env_code
   project_code              = var.project_code
   aws_region                = var.aws_region
-  availability_zone         = var.availability_zone
+  availability_zone         = data.aws_availability_zones.available.names
   vpc_cidr_block            = var.vpc_cidr_block
   private_subnet_cidr_block = var.private_subnet_cidr_block
   public_subnet_cidr_block  = var.public_subnet_cidr_block
